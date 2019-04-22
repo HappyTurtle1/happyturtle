dc7fc8fce890b3cde0689566f652b35cca1e2b51
wangxincheng <wangxincheng@leqian.com>
2019-04-22 20:43:59 +0800

    专业抽取逻辑修改。

--- src/main/java/com/wxc/happyturtle/controller/PersonController.java
+++ src/main/java/com/wxc/happyturtle/controller/PersonController.java
@@ -51,7 +51,10 @@ public class PersonController {
         Integer major1 = StringUtil.parseIntegerParam(request.getParameter("major1"));
         Integer count1 = StringUtil.parseIntegerParam(request.getParameter("count1"));
 
-        if(major1 == null || count1 == null || major1 <= 0 || count1 <= 0){
+        //抽取专业数量
+        Integer count = StringUtil.parseIntegerParam(request.getParameter("count"));
+
+        if(count == null || major1 == null || count1 == null || major1 <= 0 || count1 <= 0){
             logger.warn("参数错误！");
             result.setCode(1);
             result.setMessage("参数错误！");
@@ -62,22 +65,40 @@ public class PersonController {
         person.setMajor_id(major1);
         person.setSize(count1);
 
+        //提示信息（显示哪个专业人员不足）
+        String message = "";
+
         //抽取第一个专业人员
         List<Person> personList = personService.selectPersonList(person);
 
-        //添加的专业抽取
-        Integer major2 = StringUtil.parseIntegerParam(request.getParameter("major2"));
-        Integer count2 = StringUtil.parseIntegerParam(request.getParameter("count2"));
+        if(personList != null && personList.size() < count1){
+            Major major = majorService.selectMajorById(major1);
+            message = major.getMajor_name()+"只有"+personList.size()+"人。";
+        }
 
-        if(major2 != null && major2 > 0 && count2 > 0){
-            person.setMajor_id(major2);
-            person.setSize(count2);
+        //抽取选择的专业人员
+        if(count > 1){
+            for (int i=2;i<=count;i++){
+                //添加的专业抽取
+                Integer majorN = StringUtil.parseIntegerParam(request.getParameter("major"+i));
+                Integer countN = StringUtil.parseIntegerParam(request.getParameter("count"+i));
 
-            //抽取第二个专业人员
-            List<Person> pList = personService.selectPersonList(person);
+                if(majorN != null && majorN > 0 && countN > 0){
+                    person.setMajor_id(majorN);
+                    person.setSize(countN);
 
-            for (Person person1 : pList){
-                personList.add(person1);
+                    //抽取第二个专业人员
+                    List<Person> pList = personService.selectPersonList(person);
+
+                    for (Person person1 : pList){
+                        personList.add(person1);
+                    }
+
+                    if(pList != null && pList.size() < countN){
+                        Major major = majorService.selectMajorById(majorN);
+                        message = message + major.getMajor_name()+"只有"+pList.size()+"人。";
+                    }
+                }
             }
         }
 
@@ -89,7 +110,7 @@ public class PersonController {
 
         result.setCode(0);
         result.setData(personList);
-        result.setMessage("获取成功！");
+        result.setMessage(message);
         return result;
     }
 }
